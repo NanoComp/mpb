6d6bcfbeb818e7dec843048b1fdf60617b433bd6
stevenj <none>
1998-08-07 23:35:16 -0400

    Fixed missing semicolon when not using gcc.

--- src/util/mpi_utils.h
+++ src/util/mpi_utils.h
@@ -5,12 +5,14 @@
 
 extern void mpi_die(const char *template, ...)
 #ifdef __GNUC__
-     __attribute__ ((format (printf, 1, 2)));
+     __attribute__ ((format (printf, 1, 2)))
 #endif
+;
 
 extern void mpi_one_fprintf(FILE *f, const char *template, ...)
 #ifdef __GNUC__
-     __attribute__ ((format (printf, 2, 3)));
+     __attribute__ ((format (printf, 2, 3)))
 #endif
+;
 
 #endif /* MPI_UTIL_H */
