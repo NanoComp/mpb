2d0746a5387b9dda03d595e3169f0d4562e2dbaa
stevenj <none>
1999-08-31 20:03:11 -0400

    Broke out E/H computations into subroutines, to prepare for HDF output Broke out E/H computations into subroutines, to prepare for HDF output and position-space calculations.

--- src/maxwell/maxwell_op.c
+++ src/maxwell/maxwell_op.c
@@ -24,47 +24,67 @@
 
 #include "maxwell.h"
 
-/* assign a = scale * k x v (cross product), going from transverse to
+/* assign a = v going from transverse to cartesian coordinates.  
+   Here, a = (a[0],a[1],a[2]) is in cartesian coordinates.
+   (v[0],v[vstride]) is in the transverse basis of k.m and k.n. */
+static void assign_t2c(scalar *a, const k_data k,
+		       const scalar *v, int vstride)
+{
+     scalar v0 = v[0], v1 = v[vstride];
+
+     ASSIGN_SCALAR(a[0],
+		   SCALAR_RE(v0)*k.mx + SCALAR_RE(v1)*k.nx,
+		   SCALAR_IM(v0)*k.mx - SCALAR_IM(v1)*k.nx);
+     ASSIGN_SCALAR(a[1],
+		   SCALAR_RE(v0)*k.my + SCALAR_RE(v1)*k.ny,
+		   SCALAR_IM(v0)*k.my - SCALAR_IM(v1)*k.ny);
+     ASSIGN_SCALAR(a[2],
+		   SCALAR_RE(v0)*k.mz + SCALAR_RE(v1)*k.nz,
+		   SCALAR_IM(v0)*k.mz - SCALAR_IM(v1)*k.nz);
+}
+
+/* assign a = k x v (cross product), going from transverse to
    cartesian coordinates.
   
    Here, a = (a[0],a[1],a[2]) and k = (k.kx,k.ky,k.kz) are in
    cartesian coordinates.  (v[0],v[vstride]) is in the transverse basis of
    k.m and k.n. */
 static void assign_cross_t2c(scalar *a, const k_data k,
-			     const scalar *v, int vstride,
-			     real scale)
+			     const scalar *v, int vstride)
 {
      scalar v0 = v[0], v1 = v[vstride];
 
      /* Note that k x m = |k| n, k x n = - |k| m.  Therefore,
         k x v = k x (v0 m + v1 n) = (v0 n - v1 m) * |k|. */
 
-     scale *= k.kmag;
-
      ASSIGN_SCALAR(a[0],
-		   (SCALAR_RE(v0)*k.nx - SCALAR_RE(v1)*k.mx) * scale,
-		   (SCALAR_IM(v0)*k.nx - SCALAR_IM(v1)*k.mx) * scale);
+		   (SCALAR_RE(v0)*k.nx - SCALAR_RE(v1)*k.mx) * k.kmag,
+		   (SCALAR_IM(v0)*k.nx - SCALAR_IM(v1)*k.mx) * k.kmag);
      ASSIGN_SCALAR(a[1],
-		   (SCALAR_RE(v0)*k.ny - SCALAR_RE(v1)*k.my) * scale,
-		   (SCALAR_IM(v0)*k.ny - SCALAR_IM(v1)*k.my) * scale);
+		   (SCALAR_RE(v0)*k.ny - SCALAR_RE(v1)*k.my) * k.kmag,
+		   (SCALAR_IM(v0)*k.ny - SCALAR_IM(v1)*k.my) * k.kmag);
      ASSIGN_SCALAR(a[2],
-		   (SCALAR_RE(v0)*k.nz - SCALAR_RE(v1)*k.mz) * scale,
-		   (SCALAR_IM(v0)*k.nz - SCALAR_IM(v1)*k.mz) * scale);
+		   (SCALAR_RE(v0)*k.nz - SCALAR_RE(v1)*k.mz) * k.kmag,
+		   (SCALAR_IM(v0)*k.nz - SCALAR_IM(v1)*k.mz) * k.kmag);
 
 #ifdef DEBUG
-     scale = SCALAR_NORMSQR(a[0])+SCALAR_NORMSQR(a[1])+SCALAR_NORMSQR(a[2]);
-     CHECK(!BADNUM(scale), "yikes, crazy number!");
+     {
+	  real num;
+	  num = SCALAR_NORMSQR(a[0])+SCALAR_NORMSQR(a[1])+SCALAR_NORMSQR(a[2]);
+	  CHECK(!BADNUM(num), "yikes, crazy number!");
+     }
 #endif
 }
 
-/* assign v = k x a (cross product), going from cartesian to
+/* assign v = scale * k x a (cross product), going from cartesian to
    transverse coordinates.
   
    Here, a = (a[0],a[1],a[2]) and k = (k.kx,k.ky,k.kz) are in
    cartesian coordinates.  (v[0],v[vstride]) is in the transverse basis of
    k.m and k.n. */
 static void assign_cross_c2t(scalar *v, int vstride,
-			     const k_data k, const scalar *a)
+			     const k_data k, const scalar *a,
+			     real scale)
 {
      scalar a0 = a[0], a1 = a[1], a2 = a[2];
      scalar at0, at1;
@@ -81,12 +101,13 @@ static void assign_cross_c2t(scalar *v, int vstride,
 
      /* Now, k x a = k x (at0*m + at1*n) = (at0*n - at1*m) * |k|. */
 
+     scale *= k.kmag;  /* combine scale factor and |k|*/
      ASSIGN_SCALAR(v[0],
-		   - k.kmag * SCALAR_RE(at1),
-		   - k.kmag * SCALAR_IM(at1));
+		   - scale * SCALAR_RE(at1),
+		   - scale * SCALAR_IM(at1));
      ASSIGN_SCALAR(v[vstride],
-		   k.kmag * SCALAR_RE(at0),
-		   k.kmag * SCALAR_IM(at0));
+		   scale * SCALAR_RE(at0),
+		   scale * SCALAR_IM(at0));
 
 #ifdef DEBUG
      {
@@ -176,6 +197,103 @@ static void assign_matrix_vector(scalar_complex *newv,
 #endif
 }
 
+/* compute the D field in position space from Xin, which holds
+   the H field in Fourier space, for the specified bands.
+   The output array, dfield, is N x cur_num_bands x 3, where N
+   is the spatial indices and 3 is the field components. */
+void maxwell_compute_dfield(maxwell_data *d, evectmatrix Xin, 
+			    scalar_complex *dfield,
+			    int cur_band_start, int cur_num_bands)
+{
+     scalar *fft_data = (scalar *) dfield;
+     int i, j, b;
+
+     CHECK(Xin.c == 2, "fields don't have 2 components!");
+     CHECK(d, "null maxwell data pointer!");
+     CHECK(dfield, "null field output data!");
+     CHECK(cur_band_start >= 0 && cur_band_start + cur_num_bands <= Xin.p,
+	   "invalid range of bands for computing fields");
+
+     /* first, compute fft_data = curl(Xin): */
+     for (i = 0; i < d->other_dims; ++i)
+	  for (j = 0; j < d->last_dim; ++j) {
+	       int ij = i * d->last_dim + j;
+	       int ij2 = i * d->last_dim_size + j;
+	       k_data cur_k = d->k_plus_G[ij];
+	       
+	       for (b = 0; b < cur_num_bands; ++b)
+		    assign_cross_t2c(&fft_data[3 * (ij2*cur_num_bands 
+						    + b)], 
+				     cur_k, 
+				     &Xin.data[ij * 2 * Xin.p + 
+					      b + cur_band_start],
+				     Xin.p);
+	  }
+
+     /* now, convert to position space via FFT: */
+     compute_fft(+1, d, fft_data,
+		 cur_num_bands*3, cur_num_bands*3, 1);
+}
+
+/* Compute E (output in dfield) from D (input in dfield).  dfield
+   is in position space and corresponds to the output from
+   maxwell_compute_dfield, above. */
+void maxwell_compute_e_from_d(maxwell_data *d,
+			      scalar_complex *dfield,
+			      int cur_num_bands)
+{
+     int i, b;
+
+     CHECK(d, "null maxwell data pointer!");
+     CHECK(dfield, "null field input/output data!");
+
+     for (i = 0; i < d->fft_output_size; ++i) {
+	  symmetric_matrix eps_inv = d->eps_inv[i];
+	  for (b = 0; b < cur_num_bands; ++b) {
+	       int ib = 3 * (i * cur_num_bands + b);
+	       assign_matrix_vector(&dfield[ib], eps_inv, &dfield[ib]);
+	  }
+     }	  
+}
+
+/* Compute H field in position space from Xin.  Parameters and output
+   formats are the same as for compute_dfield, above. */
+void maxwell_compute_hfield(maxwell_data *d, evectmatrix Xin, 
+			    scalar_complex *hfield,
+			    int cur_band_start, int cur_num_bands)
+{
+     scalar *fft_data = (scalar *) hfield;
+     int i, j, b;
+
+     CHECK(Xin.c == 2, "fields don't have 2 components!");
+     CHECK(d, "null maxwell data pointer!");
+     CHECK(hfield, "null field output data!");
+     CHECK(cur_band_start >= 0 && cur_band_start + cur_num_bands <= Xin.p,
+	   "invalid range of bands for computing fields");
+
+     /* first, compute fft_data = Xin, with the vector field converted 
+	from transverse to cartesian basis: */
+     for (i = 0; i < d->other_dims; ++i)
+	  for (j = 0; j < d->last_dim; ++j) {
+	       int ij = i * d->last_dim + j;
+	       int ij2 = i * d->last_dim_size + j;
+               k_data cur_k = d->k_plus_G[ij];
+	       
+	       for (b = 0; b < cur_num_bands; ++b)
+		    assign_t2c(&fft_data[3 * (ij2*cur_num_bands 
+					      + b)], 
+			       cur_k,
+			       &Xin.data[ij * 2 * Xin.p + 
+					b + cur_band_start],
+			       Xin.p);
+	  }
+
+     /* now, convert to position space via FFT: */
+     compute_fft(+1, d, fft_data,
+		 cur_num_bands*3, cur_num_bands*3, 1);
+}
+
+
 #define MIN2(a,b) ((a) < (b) ? (a) : (b))
 
 /* Compute Xout = curl(1/epsilon * curl(Xin)) */
@@ -188,12 +306,12 @@ void maxwell_operator(evectmatrix Xin, evectmatrix Xout, void *data,
      scalar_complex *cdata;
      real scale;
      int i, j, b;
-     real *e_field_sums = 0;
      
      CHECK(d, "null maxwell data pointer!");
      CHECK(Xin.c == 2, "fields don't have 2 components!");
 
      fft_data = d->fft_data;
+     cdata = (scalar_complex *) fft_data;
 
      scale = -1.0 / Xout.N;  /* scale factor to normalize FFT; 
 				negative sign comes from 2 i's from curls */
@@ -203,45 +321,18 @@ void maxwell_operator(evectmatrix Xin, evectmatrix Xout, void *data,
 	  int cur_num_bands = MIN2(d->num_fft_bands, Xin.p - cur_band_start);
 
 	  /********************************************/
-	  
-	  /* first, compute fft_data = curl(Xin): */
-	  for (i = 0; i < d->other_dims; ++i)
-	       for (j = 0; j < d->last_dim; ++j) {
-		    int ij = i * d->last_dim + j;
-		    int ij2 = i * d->last_dim_size + j;
-		    k_data cur_k = d->k_plus_G[ij];
+	  /* compute E = 1/epsilon * curl(H), in position space. */
 
-		    for (b = 0; b < cur_num_bands; ++b)
-			 assign_cross_t2c(&fft_data[3 * (ij2*cur_num_bands 
-							 + b)], 
-					  cur_k, 
-					  &Xin.data[ij * 2 * Xin.p + 
-						    b + cur_band_start],
-					  Xin.p, scale);
-	       }
+	  maxwell_compute_dfield(d, Xin, cdata, cur_band_start, cur_num_bands);
+	  maxwell_compute_e_from_d(d, cdata, cur_num_bands);
 
 	  /********************************************/
-
-	  /* Now, multiply fft_data by 1/epsilon using FFT/IFFT pair. */
-
-	  compute_fft(+1, d, fft_data,
-		      cur_num_bands*3, cur_num_bands*3, 1);
-
-	  cdata = (scalar_complex *) fft_data;
-
-	  for (i = 0; i < d->fft_output_size; ++i) {
-	       symmetric_matrix eps_inv = d->eps_inv[i];
-	       for (b = 0; b < cur_num_bands; ++b) {
-		    int ib = 3 * (i * cur_num_bands + b);
-		    assign_matrix_vector(&cdata[ib], eps_inv, &cdata[ib]);
-	       }
-	  }
-	  
+	  /* convert back to Fourier space */
 	  compute_fft(-1, d, fft_data, cur_num_bands*3, cur_num_bands*3, 1);
 	  
 	  /********************************************/
-	  
-	  /* finally, compute Xout = curl(fft_data): */
+	  /* finally, compute Xout = curl(fft_data) (* scale factor): */
+
 	  for (i = 0; i < d->other_dims; ++i)
 	       for (j = 0; j < d->last_dim; ++j) {
 		    int ij = i * d->last_dim + j;
@@ -254,7 +345,8 @@ void maxwell_operator(evectmatrix Xin, evectmatrix Xout, void *data,
 					  Xout.p,
 					  cur_k, 
 					  &fft_data[3 * (ij2*cur_num_bands
-							 + b)]);
+							 + b)],
+					  scale);
 	       }
 	  
 	  /********************************************/
