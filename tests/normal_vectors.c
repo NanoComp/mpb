fc9a888a0007359d662894686401a5a41a7f8b52
stevenj <stevenj@fftw.org>
2005-06-03 17:37:20 -0400

    some fixes for exceptional cases in normal_vectors test

--- tests/normal_vectors.c
+++ tests/normal_vectors.c
@@ -24,7 +24,8 @@ double mydrand(void)
 /* return the angle, in degrees, between two unit-normalized vectors */
 double angle(vector3 v1, vector3 v2)
 {
-     return 180/K_PI * acos(vector3_dot(v1,v2));
+     double theta = 180/K_PI * acos(vector3_dot(v1,v2));
+     return (theta > 90 ? 180 - theta : theta);
 }
 
 /* return a random unit vector, uniformly distributed over a sphere */
@@ -42,6 +43,7 @@ vector3 random_unit_vector3(void)
      return v;
 }
 
+
 int main(void)
 {
      int i, j, nsq, num_sq_pts[] = { 12, 50, 72 };
@@ -49,12 +51,13 @@ int main(void)
 
      srand(time(NULL));
 
-     printf("Testing spherical quadratures to find normals to %d surfaces.\n",
+     printf("Testing spherical quadratures for normals to %d surfaces.\n\n",
 	    NUM_PLANES);
 
      for (nsq = 0; nsq < sizeof(num_sq_pts) / sizeof(int); ++nsq) {
-	  double err_mean, err_std;
+	  double err_mean, err_std, err_max;
 	  double min_angle = 360;
+	  int missed;
 
 	  spherical_quadrature_points(x,y,z,w, num_sq_pts[nsq]);
 
@@ -66,14 +69,15 @@ int main(void)
 		    v1.x = x[i]; v1.y = y[i]; v1.z = z[i];
 		    v2.x = x[j]; v2.y = y[j]; v2.z = z[j];
 		    a = angle(v1,v2);
-		    if (a < min_angle)
+		    if (a < min_angle && a > 1e-6)
 			 min_angle = a;
 	       }
 	  printf("%d-point formula: minimum angle is %g degrees.\n",
 		 num_sq_pts[nsq], min_angle);
 
 	  /* Normals to planes: */
-	  err_mean = err_std = 0.0;
+	  err_mean = err_std = err_max = 0.0;
+	  missed = 0;
 	  for (i = 0; i < NUM_PLANES; ++i) {
 	       vector3 n, nsum = {0,0,0};
 	       double d;
@@ -88,10 +92,15 @@ int main(void)
 		    val *= w[j];
 		    nsum = vector3_plus(nsum, vector3_scale(val, v));
 	       }
-	       nsum =  unit_vector3(nsum);
+	       if (vector3_norm(nsum) < 1e-6) {
+		    ++missed; --i;
+		    continue;
+	       }
+	       nsum = unit_vector3(nsum);
 	       {  /* stable one-pass formula for mean and std. deviation: */
 		    double e, dev;
 		    e = angle(n, nsum);
+		    if (e > err_max) err_max = e;
 		    dev = (e - err_mean) / (i + 1);
 		    err_mean += dev;
 		    err_std += i*(i+1) * dev*dev;
@@ -99,21 +108,22 @@ int main(void)
 	  }
 	  err_std = sqrt(err_std / (NUM_PLANES - 1));	  
 	  printf("planes: mean error angle for %d-pt formula = "
-		 "%g +/- %g degrees\n", 
-		 num_sq_pts[nsq], err_mean, err_std);
+		 "%g +/- %g degrees, max error = %g degrees\n", 
+		 num_sq_pts[nsq], err_mean, err_std, err_max);
+	  printf("(Fraction missed = %g)\n", 
+		 missed * 1.0 / (NUM_PLANES + missed));
 
 	  /* Normals to spheres: */
 	  err_mean = err_std = 0.0;
+	  missed=0;
 	  for (i = 0; i < NUM_PLANES; ++i) {
 	       vector3 n, nsum = {0,0,0}, c;
 	       double r, d;
 	       int j;
 	       
 	       n = random_unit_vector3();
-	       d = mydrand();
-	       do {
-		    r = mydrand() * 10; /* radius of the sphere */
-	       } while (r + d < 1.0); /* require sphere to intersect surface */
+	       d = mydrand() * 0.8 + 0.1;
+	       r = 1.0 + mydrand() * 10; /* radius of the sphere */
 	       c = vector3_scale(r + d, n);  /* center of the sphere */
 	       for (j = 0; j < num_sq_pts[nsq]; ++j) {
 		    vector3 v;
@@ -124,9 +134,14 @@ int main(void)
 		    nsum = vector3_plus(nsum, vector3_scale(val, v));
 	       }
 	       nsum =  unit_vector3(nsum);
+	       if (vector3_norm(nsum) < 1e-6) {
+		    --i;
+		    continue;
+	       }
 	       {  /* stable one-pass formula for mean and std. deviation: */
 		    double e, dev;
 		    e = angle(n, nsum);
+		    if (e > err_max) err_max = e;
 		    dev = (e - err_mean) / (i + 1);
 		    err_mean += dev;
 		    err_std += i*(i+1) * dev*dev;
@@ -134,8 +149,10 @@ int main(void)
 	  }
 	  err_std = sqrt(err_std / (NUM_PLANES - 1));	  
 	  printf("spheres: mean error angle for %d-pt formula = "
-		 "%g +/- %g degrees\n", 
-		 num_sq_pts[nsq], err_mean, err_std);
+		 "%g +/- %g degrees, max error = %g degrees\n", 
+		 num_sq_pts[nsq], err_mean, err_std, err_max);
+
+	  printf("\n");
      }
 
      return EXIT_SUCCESS;
