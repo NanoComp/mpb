f0cdd3be15d0d20d53fb159b2b0ab71a17eccab1
stevenj <stevenj@fftw.org>
2005-06-06 12:30:19 -0400

    test new normal_to_object function in libctlgeom

--- tests/normal_vectors.c
+++ tests/normal_vectors.c
@@ -8,23 +8,36 @@
 
 #include <maxwell.h>
 #include <ctl.h>
+#include <ctlgeom.h>
 
 /* return a random number in [0,1]: */
-double mydrand(void)
+static double mydrand(void)
 {
      double d = rand();
      return (d / (double) RAND_MAX);
 }
 
+/* return a uniform random number in [a,b] */
+static double myurand(double a, double b)
+{
+     return ((b - a) * mydrand() + a);
+}
+
 #define MAX_NSQ_PTS 72
 #define NUM_PLANES 100000
+#define NUM_OBJECTS 5
 
 #define K_PI 3.141592653589793238462643383279502884197
 
-/* return the angle, in degrees, between two unit-normalized vectors */
-double angle(vector3 v1, vector3 v2)
+static double pin(double x, double a, double b)
 {
-     double theta = 180/K_PI * acos(vector3_dot(v1,v2));
+     return (x < a ? a : (x > b ? b : x));
+}
+
+/* return the angle, in degrees, between two unit-normalized vectors */
+static double angle(vector3 v1, vector3 v2)
+{
+     double theta = 180/K_PI * acos(pin(vector3_dot(v1,v2), -1,1));
      return (theta > 90 ? 180 - theta : theta);
 }
 
@@ -43,6 +56,112 @@ vector3 random_unit_vector3(void)
      return v;
 }
 
+double find_edge(geometric_object o, vector3 dir, double max, double tol)
+{
+     double min = 0;
+     CHECK(point_in_fixed_objectp(vector3_scale(min, dir), o) &&
+	   !point_in_fixed_objectp(vector3_scale(max, dir), o),
+	   "object out of bounds in find_edge");
+     do {
+	  double d = (min + max) / 2;
+          if (point_in_fixed_objectp(vector3_scale(d, dir), o))
+	       min = d;
+	  else
+	       max = d;
+     } while (max - min > tol);
+     return (min + max) / 2;
+}
+
+double normal_err_to_object(geometric_object o, double r, vector3 dir,
+			    int numsq, real *x, real *y, real *z, real *w)
+{
+     int i;
+     double d;
+     vector3 c, nsum = {0,0,0};
+     dir = unit_vector3(dir);
+     d = find_edge(o, dir, 2, r * 0.01);
+     c = vector3_scale(d, dir);
+     for (i = 0; i < numsq; ++i) {
+	  vector3 v;
+	  v.x = x[i] * r;
+	  v.y = y[i] * r;
+	  v.z = z[i] * r;
+	  if (point_in_fixed_objectp(vector3_plus(v, c), o))
+	       nsum = vector3_plus(nsum, vector3_scale(w[i], v));
+     }
+     CHECK(vector3_norm(nsum) > 1e-6, "couldn't get normal vector");
+     return angle(unit_vector3(normal_to_object(c, o)), unit_vector3(nsum));
+}
+
+void normals_to_object(geometric_object o, double r, int ntrials,
+		       int numsq, real *x, real *y, real *z, real *w)
+{
+     int i;
+     double err_mean = 0, err_std = 0, err_max = 0;
+     display_geometric_object_info(0, o);
+     for (i = 0; i < ntrials; ++i) {
+	  double dev;
+	  double e = normal_err_to_object(o, r, random_unit_vector3(),
+					  numsq, x, y, z, w);
+	  if (e > err_max) err_max = e;
+	  /* stable one-pass formula for mean and std. deviation: */
+	  dev = (e - err_mean) / (i + 1);
+	  err_mean += dev;
+	  err_std += i*(i+1) * dev*dev;
+     }
+     err_std = sqrt(err_std / (ntrials - 1));
+     printf("mean error angle for %d-pt formula = "
+	    "%g +/- %g degrees, max error = %g degrees\n\n",
+	    numsq, err_mean, err_std, err_max);
+}
+
+static vector3 make_vector3(double x, double y, double z)
+{
+     vector3 v;
+     v.x = x; v.y = y; v.z = z;
+     return v;
+}
+
+/* return a random geometric object, centered at the origin, with
+   diameter roughly 1 */
+geometric_object random_object(void)
+{
+     material_type m = { 0 };
+     vector3 c = { 0, 0, 0 };
+     geometric_object o;
+     switch (rand() % 5) {
+	 case 0:
+	      o = make_sphere(m, c, myurand(0.5,1.5));
+	      break;
+	 case 1:
+	      o = make_cylinder(m, c, myurand(0.5,1.5), myurand(0.5,1.5),
+				random_unit_vector3());
+	      break;
+	 case 2:
+	      o = make_cone(m, c, myurand(0.5,1.5), myurand(0.5,1.5),
+			    random_unit_vector3(), myurand(0.5,1.5));
+	      break;
+	 case 3:
+	      o = make_block(m, c, 
+			     random_unit_vector3(),
+			     random_unit_vector3(),
+			     random_unit_vector3(),
+			     make_vector3(myurand(0.5,1.5),
+					  myurand(0.5,1.5),
+					  myurand(0.5,1.5)));
+	      break;
+	 case 4:
+	      o = make_ellipsoid(m, c, 
+				 random_unit_vector3(),
+				 random_unit_vector3(),
+				 random_unit_vector3(),
+				 make_vector3(myurand(0.5,1.5),
+					      myurand(0.5,1.5),
+					      myurand(0.5,1.5)));
+	      break;
+     }
+     return o;
+}
 
 int main(void)
 {
@@ -152,7 +271,16 @@ int main(void)
 		 "%g +/- %g degrees, max error = %g degrees\n", 
 		 num_sq_pts[nsq], err_mean, err_std, err_max);
 
+	  
 	  printf("\n");
+	  
+	  for (i = 0; i < NUM_OBJECTS; ++i) {
+	       geometric_object o = random_object();
+	       normals_to_object(o, 0.01, NUM_PLANES/100, num_sq_pts[nsq],x,y,z,w);
+	       geometric_object_destroy(o);
+	  }
+
+	  printf("-------------------------------------------------------\n");
      }
 
      return EXIT_SUCCESS;
