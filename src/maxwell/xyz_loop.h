0d6a3cb450c681b561ec9d9fc2df81e862609d8b
Steven G. Johnson <stevenj@alum.mit.edu>
2017-09-22 15:40:34 -0400

    fix --with-mpi --with-inv-symmetry typo

--- src/maxwell/xyz_loop.h
+++ src/maxwell/xyz_loop.h
@@ -43,7 +43,7 @@
           for (i1_ = 0; i1_ < n_other; ++i1_) \
      	  for (i2_ = 0; i2_ < n_last; ++i2_) { \
                  int xyz_index = i1_ * n_last + i2_; \
-          	  switch (rank) { \
+          	 switch (rank) { \
           	      case 2: i1 = i1_; i2 = i2_; i3 = 0; break; \
           	      case 3: i1 = i1_ / n2; i2 = i1_ % n2; i3 = i2_; break; \
           	      default: i1 = i2_; i2 = i3 = 0;  break; \
@@ -54,10 +54,10 @@
           int n1 = md->nx, n2 = md->ny, n3 = md->nz, i1, i2_, i3; \
           int local_n2 = md->local_ny, local_y_start = md->local_y_start; \
           int local_n3 = n3 > 1 ? md->last_dim_size / 2 : 1; \
-          for (i2_ = 0; i2_ < local_n2; ++i2_ \
+          for (i2_ = 0; i2_ < local_n2; ++i2_)               \
                for (i1 = 0; i1 < n1; ++i1) \
      	       for (i3 = 0; i3 < local_n3; ++i3) { \
-                      int i2 = i2_ + local_y_start; \
+                  int i2 = i2_ + local_y_start; \
                	  int xyz_index = ((i2_ * n1 + i1) * local_n3 + i3);
 #  endif  /* HAVE_MPI */
 
