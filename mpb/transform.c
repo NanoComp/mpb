6a386f64e43df202c964f9141787a014ec3111eb
tchr <tchr@mit.edu>
2020-02-12 15:18:08 -0500

    Fix bug in vector transformation and account for the fact that vector fields are in a Cartesian basis

--- mpb/transform.c
+++ mpb/transform.c
@@ -45,7 +45,7 @@ cnumber transformed_overlap(matrix3x3 W, vector3 w)
     cnumber integral = {0,0};
     number detW;
     vector3 kvector = cur_kvector;
-    matrix3x3 invW;
+    matrix3x3 invW, Wc;
 
     if (!curfield || !strchr("db", curfield_type)) {
         mpi_one_fprintf(stderr, "a real-space H or D-field must be loaded beforehand.\n");
@@ -65,22 +65,27 @@ cnumber transformed_overlap(matrix3x3 W, vector3 w)
     c2 = n2 <= 1 ? 0 : geometry_lattice.size.y * 0.5;
     c3 = n3 <= 1 ? 0 : geometry_lattice.size.z * 0.5;
 
-    invW = matrix3x3_inverse(W);
     detW = matrix3x3_determinant(W);
     if (fabs(fabs(detW) - 1.0) > 1e-12) {
         mpi_one_fprintf(stderr, "valid symmetry operations {W|w} must have |det(W)| = 1.0\n");
         return integral;
     }
+    invW = matrix3x3_inverse(W);
+    /* W is specified in the lattice basis, but field *vectors* evaluated in real-space are
+       in a Cartesian basis: when we transform the vector components, we must account for this
+       difference. We transform W to a Cartesian basis Wc=RWR⁻¹ (R=geometry_lattice.basis, a
+       matrix w/ columns of Cartesian basis vectors) and then use Wc to transform vector fields */
+    Wc = matrix3x3_mult(matrix3x3_mult(geometry_lattice.basis, W),
+                        matrix3x3_inverse(geometry_lattice.basis));
 
     kvector.x *= TWOPI/geometry_lattice.size.x; /* hoist these rescalings outside the  */
     kvector.y *= TWOPI/geometry_lattice.size.y; /* loop; might be that licm takes care */
     kvector.z *= TWOPI/geometry_lattice.size.z; /* of it - but better to be sure       */
 
-
     /* Loop over coordinates (introduces int vars i1, i2, i3, xyz_index) */
     LOOP_XYZ(mdata) { /* implies two opening braces '{{' */
         vector3 p, pt;
-        scalar_complex F[3], Ft[3], integrand, phase;
+        scalar_complex F[3], Ft[3], Ftemp[3], integrand, phase;
         double deltaphi;
 
         /* Current lattice position in loop */
@@ -95,17 +100,17 @@ cnumber transformed_overlap(matrix3x3 W, vector3 w)
            generality in the case of fractional translations. Unfortunately, this currently
            precludes compatibility with MPI, since get_val is not implemented for MPI */
         /* TODO: Remove disclaimer above if PR#112 is merged */
-        get_bloch_field_point_(Ft, pt); /* sets Ft to F at p [excludes exp(ikr) factor] */
+        get_bloch_field_point_(Ftemp, pt); /* assigns Ftemp to field at p [excludes exp(ikr) factor] */
 
-        /* Transform the vector components of Ft by W; a bit tedious to do, since 
-           there are no matrix3x3*cvector3 routines, nor any CASSIGN_CVECTOR_RE/IM.
-           Instead, we do manually what is done in matrix3x3_vector3_mult, twice  */
-        Ft[0].re = W.c0.x*Ft[0].re + W.c1.x*Ft[1].re + W.c2.x*Ft[2].re;
-        Ft[0].im = W.c0.x*Ft[0].im + W.c1.x*Ft[1].im + W.c2.x*Ft[2].im;
-        Ft[1].re = W.c0.y*Ft[0].re + W.c1.y*Ft[1].re + W.c2.y*Ft[2].re;
-        Ft[1].im = W.c0.y*Ft[0].im + W.c1.y*Ft[1].im + W.c2.y*Ft[2].im;
-        Ft[2].re = W.c0.z*Ft[0].re + W.c1.z*Ft[1].re + W.c2.z*Ft[2].re;
-        Ft[2].im = W.c0.z*Ft[0].im + W.c1.z*Ft[1].im + W.c2.z*Ft[2].im;
+        /* Transform the vector components of Ftemp by W to obtain Ft; a bit repetitious but 
+           we just write out the matrix-product manually here, for real and imag components.
+           Since F, Ftemp, and Ft are in a Cartesian basis, we use Wc instead of W.          */
+        Ft[0].re = Wc.c0.x*Ftemp[0].re + Wc.c1.x*Ftemp[1].re + Wc.c2.x*Ftemp[2].re;
+        Ft[0].im = Wc.c0.x*Ftemp[0].im + Wc.c1.x*Ftemp[1].im + Wc.c2.x*Ftemp[2].im;
+        Ft[1].re = Wc.c0.y*Ftemp[0].re + Wc.c1.y*Ftemp[1].re + Wc.c2.y*Ftemp[2].re;
+        Ft[1].im = Wc.c0.y*Ftemp[0].im + Wc.c1.y*Ftemp[1].im + Wc.c2.y*Ftemp[2].im;
+        Ft[2].re = Wc.c0.z*Ftemp[0].re + Wc.c1.z*Ftemp[1].re + Wc.c2.z*Ftemp[2].re;
+        Ft[2].im = Wc.c0.z*Ftemp[0].im + Wc.c1.z*Ftemp[1].im + Wc.c2.z*Ftemp[2].im;
         
         /* Get the Bloch field value at current point p [exludes exp(ikr) factor].
            We multiply the input field F (either B or D-field) with μ⁻¹ or ε⁻¹ to get
